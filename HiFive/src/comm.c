0f27783d53df8a7a7c74877885711333a43ebc9d
Mario Simental <simentalma@yahoo.com>
2024-04-29 12:41:39 -0500

    Fixed timings.

--- HiFive/src/comm.c
+++ HiFive/src/comm.c
@@ -4,75 +4,78 @@
 
 #include "eecs388_lib.h"
 
-#define Flashing_period 100
+#define FLASHING_PERIOD 100
+#define SERVO_PULSE_MAX 2400 /* 2400 us */
+#define SERVO_PULSE_MIN 544 /* 544 us */
+#define SERVO_PERIOD 20000 /* 20000 us (20ms) */
+
+uint16_t dist = 0;
 
 void auto_brake(int devid)
 {
     // Task-1: 
     // Your code here (Use Lab 02 - Lab 04 for reference)
     // Use the directions given in the project document
+    
+    // TFmini lidar
+    // The sensor's data is encoded in a 9-byte data frame. THe first two two bytes are frame headers.
+    // The next two bytes encode the actual distance data (cm) which are the low and high byte of the 16 bit distance data.
+    // The remaining bits are not used thus are 'emptied' by reading five times.
 
-    uint16_t dist = 0;
+    while(1){ // Ensure auto_brake occurs since steering might cause timing issues.
+        if ('Y' == ser_read(devid) && 'Y' == ser_read(devid)) { // Check the header frames.
 
-    while (1) {
+            dist = ser_read(devid); // Get the low byte of the distance data.
+            dist |= ser_read(devid) << 8; // Get the high byte of the distance data and shift the bits the left.
 
-    if ('Y' == ser_read(0) && 'Y' == ser_read(0)) {
-        dist = ser_read(0);
-        dist |= ser_read(0) << 8;
+            //printf("\ndistance=%d", dist); // Debugging purposes
 
-        for(int i = 0; i < 5; i++) {
-            ser_read(0);
+            // Get the remaining data and 'empty' it.
+            for(int i = 0; i < 5; i++) { 
+                ser_read(devid);
+            }
+
+            if (dist > 200) {
+                gpio_write(RED_LED, OFF);
+                gpio_write(GREEN_LED, ON);
+            } else if (dist > 100) {
+                gpio_write(RED_LED, ON);
+                gpio_write(GREEN_LED, ON);
+            } else if (dist > 60) {
+                gpio_write(GREEN_LED, OFF);
+                gpio_write(RED_LED, ON);
+            } else {
+                gpio_write(GREEN_LED, OFF);
+                gpio_write(RED_LED, ON);
+                delay(FLASHING_PERIOD / 2);
+                gpio_write(RED_LED, OFF);
+                delay(FLASHING_PERIOD / 2);
+            }    
+            return;
         }
-
-        if (dist > 200) {
-            gpio_write(RED_LED, OFF);
-            gpio_write(GREEN_LED, ON);
-        } 
-
-        else if (dist > 100) {
-            gpio_write(RED_LED, ON);
-            gpio_write(GREEN_LED, ON);
-        }
-
-        else if (dist > 60) {
-            gpio_write(RED_LED, ON);
-            gpio_write(GREEN_LED, OFF);
-        }
-
-        else {
-            for (int i = 0; i < 10; i++) { 
-                    gpio_write(RED_LED, ON);
-                    delay(Flashing_period / 2);
-                    gpio_write(RED_LED, OFF);
-                    delay(Flashing_period / 2);
-                }
-        }
-
-        printf("Measured distance: %d cm\n", dist);
-    }
     }
 }
 
+
 int read_from_pi(int devid)
 {
-// Task-2:
-// You code goes here (Use Lab 09 for reference)
-// After performing Task-2 at dnn.py code, modify this part to read angle values from Raspberry Pi.
+    // Task-2:
+    // You code goes here (Use Lab 09 for reference)
+    // After performing Task-2 at dnn.py code, modify this part to read angle values from Raspberry Pi.
 
-    char buffer[10];
-    int angle, read_data;
-
-   
-    read_data = ser_readline(devid, sizeof(buffer), buffer);
-
-    
-    if (read_data > 0) {
-        sscanf(buffer, "%f", &angle);
-        printf("From PI to HIFIVE: read(%d) => %d \n", read_data, angle);
-        return angle;
-    } else {
-        
-        return -1; 
+    char buffer[8]; // A character array of size 8 used to store data read from the serial port.
+    int angle, read_data; // Angle stores the converted integer read from the buffer, 
+                          // and read_data stores the number of bytes read from the serial port.
+    if(ser_isready(devid)){ // Check if the serial port is ready.
+        read_data = ser_readline(devid, sizeof(buffer), buffer); // Read data from the serial port given the 
+                                                                 // device id, buffer size, and buffer.
+        if(read_data > 0) {
+            sscanf(buffer, "%d", &angle); // Parses data from buffer and stores it in angle.
+            printf("\nFrom PI to HIFIVE: read(%d) => %d \n", read_data, angle);
+            return angle; // Returns the angle.          
+        }else{
+            return -1;
+        }
     }
 }
 
@@ -82,8 +85,20 @@ void steering(int gpio, int pos)
     // Your code goes here (Use Lab 05 for reference)
     // Check the project document to understand the task
 
-    servo(gpio, pos);
-    
+    // Pulse Width Modulation
+    // RC servo motor operates betweens 544us (0 degrees) to 2400us (180 degrees) at a 20ms period.
+    // PWM is a way to encode analog signals using digital outputs, 
+    // and by altering the durations of ON and OFF we can encode an arbitrary analog signal.
+    if(pos > 180 || pos < 0){
+        return;
+    }
+    int pulse_width = ((SERVO_PULSE_MAX - SERVO_PULSE_MIN) * (pos/180.0)) + SERVO_PULSE_MIN;
+    //printf("\npulse width=%d", pulse_width); // Debugging purposes
+    gpio_write(gpio, ON);
+    delay_usec(pulse_width);
+    gpio_write(gpio, OFF);
+    delay_usec(SERVO_PERIOD - pulse_width);
+    //printf("\npulse width mod=%d", SERVO_PERIOD - pulse_width); // Debugging purposes
 }
 
 
@@ -108,7 +123,6 @@ int main()
     printf("Begin the main loop.\n");
 
     while (1) {
-
         auto_brake(lidar_to_hifive); // measuring distance using lidar and braking
         int angle = read_from_pi(pi_to_hifive); //getting turn direction from pi
         printf("\nangle=%d", angle) 
@@ -120,16 +134,17 @@ int main()
             // You are welcome to pass the angle values directly to the steering function.
             // If the servo function is written correctly, it should still work,
             // only the movements of the servo will be more subtle
-            if(angle>0){
+
+            /*if(angle>0){
                 steering(gpio, 180);
             }
             else {
                 steering(gpio,0);
-            }
+            }*/
             
             // Uncomment the line below to see the actual angles on the servo.
             // Remember to comment out the if-else statement above!
-            // steering(gpio, angle);
+            steering(gpio, angle);
         }
 
     }
--- task1/README.md
+++
@@ -1,1 +0,0 @@
-# eecs388_project
